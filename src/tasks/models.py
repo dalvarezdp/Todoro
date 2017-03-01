4ca93f05f5820bf4dad42cc06242c45944de7787
David Álvarez <david11988@gmail.com>
2017-03-01 21:51:38 +0100

    Creamos modelo Task, generamos migracion y lo ponemos en el administrador

--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+
+    PENDING = "PEN"
+    DONE = "DON"
+    STATUSES = (
+        (PENDING,"Pending"),
+        (DONE,"Done")
+    )
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True,null=True)
+    status = models.CharField(max_length=3, default=PENDING, choices=STATUSES)
+    time_estimated = models.IntegerField(null=True)
+    deadline = models.DateField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True) #Añade automáticamente la fecha de creación
+    modified_at = models.DateTimeField(auto_now=True) #Actualiza la fecha al guardar automáticamente
+
+    def __str__(self):
+        return self.name
